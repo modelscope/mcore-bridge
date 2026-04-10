a48d08b805745eda67e241a7ef46b9b7405a7892
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-04-10 11:00:06 +0800

    update

--- src/mcore_bridge/bridge/gpt_bridge.py
+++ src/mcore_bridge/bridge/gpt_bridge.py
@@ -274,6 +274,8 @@ class GPTBridge:
                     else:
                         if '.lora_A.' in k or '.lora_B.' in k or '.original_module.' in k:
                             continue
+                        if '.modules_to_save.' in k and f'.{self._adapter_name}.' not in k:
+                            continue
                         k = k.replace('.base_layer.', '.')
                         k = k.replace(f'.modules_to_save.{self._adapter_name}.', '.')
                         new_state_dict[k] = v
