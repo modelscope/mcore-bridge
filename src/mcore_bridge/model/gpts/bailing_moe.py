d10ced2be5de437a7b98d02727fb6089f03d16f0
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-04-29 14:49:22 +0800

    update

--- src/mcore_bridge/model/gpts/bailing_moe.py
+++ src/mcore_bridge/model/gpts/bailing_moe.py
@@ -7,9 +7,15 @@ from ..register import ModelMeta, register_model
 
 class BailingMoeBridge(GPTBridge):
     hf_embed_key = 'model.word_embeddings.weight'
+    hf_attn_prefix = 'attention'
     hf_q_norm_key = 'query_layernorm.weight'
     hf_k_norm_key = 'key_layernorm.weight'
     hf_expert_bias_key = 'gate.expert_bias'
+    hf_o_proj_key = 'dense'
+
+    def _set_qkv(self, mg_attn, hf_state_dict, to_mcore: bool):
+        self._set_state_dict(mg_attn, 'linear_qkv.weight', hf_state_dict, 'query_key_value.weight', to_mcore)
+        return hf_state_dict
 
 
 register_model(ModelMeta(
